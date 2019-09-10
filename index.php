a7957dfa5701e4c4addd70b9606a1f6d708768f1
Christopher <cxpop@outlook.com>
2019-09-10 09:21:13 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Christopher
+ * Date: 9/9/2019
+ * Time: 1:15 PM
+ */
+$aantal = 14;
+
+
+$deze_jongen = "Deze jongen is ";
+
+
+echo $deze_jongen.$aantal." jaar oud.";
